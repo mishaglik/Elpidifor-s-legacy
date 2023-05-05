6f9f3b27311138b5b31cfcd3b4d6e9c462cd145f
Lev_Semin <semin.leva12@mail.ru>
2023-05-05 16:19:42 +0300

    fix

--- build/CMakeFiles/test-picture.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test-picture.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/levce/projectsDED/Elpidifor-s-legacy
-# Build directory: /home/levce/projectsDED/Elpidifor-s-legacy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/levce/projectsDED/Elpidifor-s-legacy/build/test-picture[1]_include.cmake")
-subdirs("_deps/googletest-build")
