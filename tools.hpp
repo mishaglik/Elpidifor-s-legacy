a8a03b824305ec15955482a693830004f6ad0c08
Lev_Semin <semin.leva12@mail.ru>
2023-05-09 18:39:07 +0300

    adding default colors for class Color

--- tools.hpp
+++ tools.hpp
@@ -161,6 +161,20 @@ namespace booba { // boot of outstanding best api
         uint8_t g;
         uint8_t b;
         uint8_t a;
+        
+        enum
+        {
+            RED    = 0xFF0000FF, 
+            ORANGE = 0xFF6600FF,
+            YELLOW = 0xFFFF00FF,
+            GREEN  = 0x00FF00FF, 
+            BLUE   = 0x0000FFFF, 
+            INDIGO = 0x480082FF,
+            VIOLET = 0x8800FFFF,
+            BLACK  = 0x000000FF,
+            WHITE  = 0xFFFFFFFF,
+            TRANSPARENT = 0x0
+        };
 
         Color (uint8_t r = 0u, uint8_t g = 0u, uint8_t b = 0u, uint8_t a = 255u):
             r(r), g(g), b(b), a(a) 
